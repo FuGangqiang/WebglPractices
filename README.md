a4243154506c46d5f41a43ad19b30830eaefd1bd
FuGangqiang <fu_gangqiang@qq.com>
2019-09-15 17:57:31 +0800

    init repo

---
+++ README.md
@@ -0,0 +1,19 @@
+# Webgl2 Practices
+
+using Rust Wasm.
+
+# Development
+
+start app:
+
+```
+wasm-pack build
+npm install
+npm run start
+```
+
+make some rust code changes:
+
+```
+wasm-pack build
+```
---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+use wasm_bindgen::prelude::*;
+
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen(start)]
+pub fn start() -> Result<(), JsValue> {
+    index()?;
+    Ok(())
+}
+
+fn index() -> Result<(), JsValue> {
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+
+    let val = document.create_element("p")?;
+    val.set_inner_html("Webgl2 Practices");
+    body.append_child(&val)?;
+
+    Ok(())
+}
